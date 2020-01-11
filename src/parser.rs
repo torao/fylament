4f0e193c58030f0f1b2a75cc2f84a9b3651f85d1
torao <koiroha@gmail.com>
2020-01-11 19:11:53 +0900

    M

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,21 @@
 extern crate nom;
 
-use nom::branch::alt;
-use nom::branch::permutation;
-use nom::bytes::complete::escaped_transform;
-use nom::bytes::complete::tag;
-use nom::character::complete::multispace0;
-use nom::combinator::map;
-use nom::combinator::not;
+use nom::branch::{alt, permutation};
+use nom::bytes::complete::{escaped_transform, tag, take_while_m_n};
+use nom::character::complete::{char, multispace0};
+use nom::character::is_hex_digit;
+use nom::combinator::{map, map_res, not, value};
 use nom::IResult;
+use nom::multi::count;
 use nom::sequence::delimited;
 
-use self::nom::bytes::complete::take_while_m_n;
-use self::nom::character::is_hex_digit;
-use self::nom::combinator::{map_res, value};
-use self::nom::multi::count;
+#[derive(Debug, Fail)]
+pub enum ParseError {
+  #[fail(display = "i/o error: {}", description)]
+  StringLiteralError {
+    description: String
+  }
+}
 
 fn parser(s: &str) -> IResult<&str, &str> {
   permutation((
@@ -36,18 +38,22 @@ fn string(s: &str) -> IResult<&str, &str> {
   delimited(
     char('\"'),
     escaped_transform(not(char('\"')), '\\', alt((
-      value(char('b'), "\x08"),
-      value(char('t'), "\x09"),
-      value(char('n'), "\x0a"),
-      value(char('f'), "\x0c"),
-      value(char('r'), "\x0d"),
-      value(char('\"'), "\""),
-      value(char('\''), "\'"),
-      value(char('\\'), "\\"),
+      value("\x08", char('b')),
+      value("\x09", char('t')),
+      value("\x0a", char('n')),
+      value("\x0c", char('f')),
+      value("\x0d", char('r')),
+      value("\"", char('\"')),
+      value("\'", char('\'')),
+      value("\\", char('\\')),
       map_res(
         permutation((char('u'), take_while_m_n(4, 4, is_hex_digit))),
         |(_, code)| code.parse::<char>(16)),
     ))),
     char('\"'),
   )(s)
+}
+
+fn hex_to_string(s:&[u8]) -> Result<String, ParseError> {
+  for i in
 }
